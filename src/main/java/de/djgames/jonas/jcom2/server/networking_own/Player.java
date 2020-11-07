ef09bbe4ee6efc33494e7f0cdc5be1bdc40a162b
JoKrus <JoKr1999@web.de>
2020-11-07 22:55:55 +0100

    objects equals removed obsolete classes
    Signed-off-by: JoKrus <JoKr1999@web.de>

--- src/main/java/de/djgames/jonas/jcom2/server/networking_own/Player.java
+++ src/main/java/de/djgames/jonas/jcom2/server/networking_own/Player.java
@@ -1,5 +1,6 @@
 package de.djgames.jonas.jcom2.server.networking_own;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Player {
@@ -26,5 +27,24 @@ public class Player {
         return playerName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Player player = (Player) o;
+
+        if (!Objects.equals(id, player.id)) return false;
+        if (!Objects.equals(communicator, player.communicator))
+            return false;
+        return Objects.equals(playerName, player.playerName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (communicator != null ? communicator.hashCode() : 0);
+        result = 31 * result + (playerName != null ? playerName.hashCode() : 0);
+        return result;
+    }
 }
